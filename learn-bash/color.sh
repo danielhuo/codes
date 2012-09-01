a23c8516437aa1344eaf3862f17ff4c0f892edf4
berlinix <my@berlinix.com>
2012-09-01 16:04:21 +0800

    add bash color output; js string split

---
+++ learn-bash/color.sh
@@ -0,0 +1,3 @@
+echo -e "\E[31mRed\033[0m"
+echo -e "\E[32;40mGreen+Black\033[0m"
+echo -e "\E[31;43mRed+Yellow\033[0m"
---
+++ learn-js/str-split.js
@@ -0,0 +1,5 @@
+s = "a,b,c,d";
+ws = s.split(",");
+for(i = 0; i < ws.length; ++i) {
+    console.log(ws[i]);
+}
